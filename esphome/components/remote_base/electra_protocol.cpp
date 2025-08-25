baa936bee2da778e85854900e88bb309d0c71ad7
Oleksii <uzerchuk@gmail.com>
2025-08-25 20:12:18 -0300

    try to change timings

--- esphome/components/remote_base/electra_protocol.cpp
+++ esphome/components/remote_base/electra_protocol.cpp
@@ -6,11 +6,11 @@ namespace remote_base {
 
 static const char *const TAG = "remote.electra";
 
-static const uint32_t HEADER_HIGH_US = 9000;
-static const uint32_t HEADER_LOW_US = 4500;
+static const uint32_t HEADER_HIGH_US = 3120;
+static const uint32_t HEADER_LOW_US = 1570;
 static const uint32_t BIT_HIGH_US = 560;
-static const uint32_t BIT_ONE_LOW_US = 1690;
-static const uint32_t BIT_ZERO_LOW_US = 560;
+static const uint32_t BIT_ONE_LOW_US = 1060;
+static const uint32_t BIT_ZERO_LOW_US = 250;
 
 void ElectraProtocol::encode(RemoteTransmitData *dst, const ElectraData &data) {
   ESP_LOGD(TAG, "Sending Electra: address=0x%04X, command=0x%04X command_repeats=%d", data.address, data.command,
