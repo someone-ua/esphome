b98ac33891f52eafb2d8e8909d7795cf566e7c28
Oleksii <uzerchuk@gmail.com>
2025-08-25 19:59:22 -0300

    try to add protocol

---
+++ esphome/components/remote_base/electra_protocol.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "remote_base.h"
+
+namespace esphome {
+namespace remote_base {
+
+struct ElectraData {
+  uint16_t address;
+  uint16_t command;
+  uint16_t command_repeats;
+
+  bool operator==(const ElectraData &rhs) const { return address == rhs.address && command == rhs.command; }
+};
+
+class ElectraProtocol : public RemoteProtocol<ElectraData> {
+ public:
+  void encode(RemoteTransmitData *dst, const ElectraData &data) override;
+  optional<ElectraData> decode(RemoteReceiveData src) override;
+  void dump(const ElectraData &data) override;
+};
+
+DECLARE_REMOTE_PROTOCOL(Electra)
+
+template<typename... Ts> class ElectraAction : public RemoteTransmitterActionBase<Ts...> {
+ public:
+  TEMPLATABLE_VALUE(uint16_t, address)
+  TEMPLATABLE_VALUE(uint16_t, command)
+  TEMPLATABLE_VALUE(uint16_t, command_repeats)
+
+  void encode(RemoteTransmitData *dst, Ts... x) override {
+    ElectraData data{};
+    data.address = this->address_.value(x...);
+    data.command = this->command_.value(x...);
+    data.command_repeats = this->command_repeats_.value(x...);
+    ElectraProtocol().encode(dst, data);
+  }
+};
+
+}  // namespace remote_base
+}  // namespace esphome
