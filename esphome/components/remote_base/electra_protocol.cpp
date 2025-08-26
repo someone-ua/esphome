078c8e1f91e43f449fa46745775ac9f87f82a11b
Oleksii <uzerchuk@gmail.com>
2025-08-26 19:53:33 -0300

    Add logging for payload value in ElectraProtocol dump function

--- esphome/components/remote_base/electra_protocol.cpp
+++ esphome/components/remote_base/electra_protocol.cpp
@@ -63,6 +63,7 @@ void ElectraProtocol::dump(const ElectraData &data) {
            data.payload.bytes[2], data.payload.bytes[3], data.payload.bytes[4], data.payload.bytes[5],
            data.payload.bytes[6], data.payload.bytes[7]);
   ESP_LOGI(TAG, "  words: %08X %08X", data.payload.words[0], data.payload.words[1]);
+  ESP_LOGI(TAG, " payload value: %016X", data.payload.value);
 }
 
 }  // namespace remote_base
