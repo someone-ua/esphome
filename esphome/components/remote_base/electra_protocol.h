f942c827d14b3efd3fd69424d2457c27fa03be17
Oleksii <uzerchuk@gmail.com>
2025-08-26 08:10:12 -0300

    bits

--- esphome/components/remote_base/electra_protocol.h
+++ esphome/components/remote_base/electra_protocol.h
@@ -11,9 +11,9 @@ struct ElectraData {
   {
       uint64_t value;
       struct __attribute__((packed)) {
-            uint64_t  padding : 42;
+            uint64_t  padding : 44;
             unsigned  temperature : 4;
-            uint64_t  something : 18;
+            uint64_t  something : 16;
       } fields;
   } payload;
 
