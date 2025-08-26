1692e0346c19702ead71c07c0ab666b96ae76196
Oleksii <uzerchuk@gmail.com>
2025-08-26 20:26:22 -0300

    Fix log formatting in dump function to include magic value in bytes log entry

--- esphome/components/remote_base/electra_protocol.cpp
+++ esphome/components/remote_base/electra_protocol.cpp
@@ -54,7 +54,8 @@ void ElectraProtocol::dump(const ElectraData &data) {
   ESP_LOGI(TAG, "Received Electra: payload=0x%016X  magic=0x%016X", data.payload.valint64, data.magic);
   ESP_LOGI(TAG, "Received Electra: magic=0x%016X, payload=0x%016X", data.magic, data.payload.valint64);
 
-  ESP_LOGI(TAG, "  bytes: %02X %02X %02X %02X %02X %02X %02X %02X", data.payload.bytes[0], data.payload.bytes[1],
+  ESP_LOGI(TAG, "magic=0x%016X,  bytes: %02X %02X %02X %02X %02X %02X %02X %02X", data.magic,
+           data.payload.bytes[0], data.payload.bytes[1],
            data.payload.bytes[2], data.payload.bytes[3], data.payload.bytes[4], data.payload.bytes[5],
            data.payload.bytes[6], data.payload.bytes[7]);
   ESP_LOGI(TAG, "  words: %08X %08X", data.payload.words[0], data.payload.words[1]);
