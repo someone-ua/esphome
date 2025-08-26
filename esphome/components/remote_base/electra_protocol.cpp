c0a4a8d47debe34f4861917cc5cda4996ba920e4
Oleksii <uzerchuk@gmail.com>
2025-08-25 21:51:52 -0300

    log

--- esphome/components/remote_base/electra_protocol.cpp
+++ esphome/components/remote_base/electra_protocol.cpp
@@ -45,6 +45,7 @@ optional<ElectraData> ElectraProtocol::decode(RemoteReceiveData src) {
     } else if (src.expect_item(BIT_HIGH_US, BIT_ZERO_LOW_US)) {
       data.value2 &= ~mask;
     } else {
+      ESP_LOGD(TAG, "Value2 decoding failed at mask 0x%04X", mask);
       break;
     }
   }
