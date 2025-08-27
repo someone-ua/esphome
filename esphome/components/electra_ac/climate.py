c3ba414dd134fa2784e0985561aae884b7a06556
Oleksii <uzerchuk@gmail.com>
2025-08-27 13:33:20 -0300

    fix variable name

--- esphome/components/electra_ac/climate.py
+++ esphome/components/electra_ac/climate.py
@@ -3,8 +3,8 @@ from esphome.components import climate_ir
 
 AUTO_LOAD = ["climate_ir"]
 
-electra_ac__ns = cg.esphome_ns.namespace("electra_ac")
-ElectraClimate = zhlt01_ns.class_("ElectraClimate", climate_ir.ClimateIR)
+electra_ac_ns = cg.esphome_ns.namespace("electra_ac")
+ElectraClimate = electra_ac_ns.class_("ElectraClimate", climate_ir.ClimateIR)
 
 CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(ElectraClimate)
 
